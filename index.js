d0839500d5069e3a0833c36e64ba3adeefc8b322
Stvlutta <stvlutta@gmail.com>
2025-03-02 18:52:19 +0300

    js file editted

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function moveDodgerLeft() {
+  let dodger = document.getElementById("dodger");
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let dodger = document.getElementById("dodger");
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left < 360) { // Assuming the container width is 400px and dodger width is 40px
+    dodger.style.left = `${left + 1}px`;
+  }
+}
